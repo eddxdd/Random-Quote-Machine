e79eff6267541fac61da223c9d8c32b4d84071f6
eddxdd <31359604+eddxdd@users.noreply.github.com>
2017-10-20 00:37:48 -0400

    Update quotes.js

--- src/js/quotes.js
+++ src/js/quotes.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
 
+$("#getQuote").on('click', function() {
+
+  $.ajaxSetup({cache: false});
+
+  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&callback=", function(data) {
+
+    $(".message").html(data[0].content + "~" + data[0].title)
+});// end of getJSON
+
+}); // end of getQuote function
+
+}); //end
