190206a183e4c33eb115bab9ba8452dfa8daf984
Eduardo Lemos <31359604+eddxdd@users.noreply.github.com>
2017-10-21 12:12:34 -0400

    Update quotes.js

--- src/js/quotes.js
+++ src/js/quotes.js
@@ -1,21 +1,27 @@
 // Random Quote Generator
+// Quote API
 var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 
-
+// Twitter
 var getQuote = function(data) {
   $(".quote-text").text(data.quoteText);
 
   var quot = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' Author ' + data.quoteAuthor;
+
   if (data.quoteAuthor === '') {
     data.quoteAuthor = 'Unknown';
   }
+
   $(".author-text").text('Author: ' + data.quoteAuthor);
   $(".twitter-share-button").attr("href", quot);
 };
+
+
 $(document).ready(function() {
   $.getJSON(url, getQuote, 'jsonp');
-
 });
-$("#quote").click(function() {
+
+// Quote on click
+$("#getQuote").click(function() {
   $.getJSON(url, getQuote, 'jsonp');
 });
